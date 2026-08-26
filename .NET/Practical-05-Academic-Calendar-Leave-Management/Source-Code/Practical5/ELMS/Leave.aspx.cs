bf51e3b8bc2da0466aab739a5f0e98c0475825e4
VivekHirani36 <vivekhirani3636@gmail.com>
2026-08-26 13:06:07 +0530

    Add Practical 5 Academic Calendar and Leave Management System

---
+++ .NET/Practical-05-Academic-Calendar-Leave-Management/Source-Code/Practical5/ELMS/Leave.aspx.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ELMS
+{
+    public partial class Leave : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Check Cookie
+                if (Request.Cookies["EmployeeUser"] == null)
+                {
+                    Response.Redirect("employee.aspx");
+                    return;
+                }
+
+                string userName = Request.Cookies["EmployeeUser"].Value;
+
+                // Store cookie value in Session
+                Session["UserName"] = userName;
+
+                // Display username
+                lblWelcome.Text = "Welcome, " + userName;
+            }
+            else
+            {
+                // Display username from Session
+                if (Session["UserName"] != null)
+                {
+                    lblWelcome.Text =
+                        "Welcome, " + Session["UserName"].ToString();
+                }
+            }
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+            lblSelectedDate.Text =
+                "Selected Date: " +
+                Calendar1.SelectedDate.ToString("dd-MM-yyyy");
+        }
+
+        protected void btnApplyLeave_Click(object sender, EventArgs e)
+        {
+            // Check Leave Type
+            if (ddlLeaveType.SelectedValue == "")
+            {
+                lblResult.Text = "Please select a Leave Type.";
+                return;
+            }
+
+            // Check Date
+            if (Calendar1.SelectedDate == DateTime.MinValue)
+            {
+                lblResult.Text = "Please select a Leave Date.";
+                return;
+            }
+
+            string userName = "";
+
+            if (Session["UserName"] != null)
+            {
+                userName = Session["UserName"].ToString();
+            }
+
+            string leaveType = ddlLeaveType.SelectedValue;
+
+            string leaveDate =
+                Calendar1.SelectedDate.ToString("dd-MM-yyyy");
+
+            lblResult.Text =
+                "Leave Applied Successfully!<br/>" +
+                "Employee Name: " + userName + "<br/>" +
+                "Leave Type: " + leaveType + "<br/>" +
+                "Leave Date: " + leaveDate;
+        }
+
+        protected void btnLogout_Click(object sender, EventArgs e)
+        {
+            // Clear Session
+            Session.Clear();
+            Session.Abandon();
+
+            // Delete Cookie
+            if (Request.Cookies["EmployeeUser"] != null)
+            {
+                Response.Cookies["EmployeeUser"].Expires =
+                    DateTime.Now.AddDays(-1);
+            }
+
+            // Go back to Login
+            Response.Redirect("employee.aspx");
+        }
+    }
+}
